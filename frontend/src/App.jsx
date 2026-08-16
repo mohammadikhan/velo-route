133f610cd50a7973faf473b3b07fa7d8e26acdac
Mohammad Khan <mohammadikhan821@gmail.com>
2026-08-16 15:49:51 -0400

    Added main.jsx and app.jsx and corresponding css files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
 import HomePage from './pages/AllRides.jsx'
 import RideDetailPage from './pages/RideDetailPage'
 import LandingPage from "./pages/LandingPage.jsx";

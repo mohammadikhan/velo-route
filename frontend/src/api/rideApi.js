cbe572f5b5c249244422cfbb09db3d19eef5d91c
Mohammad Khan <mohammadikhan821@gmail.com>
2026-08-22 12:20:09 -0400

    Created file to store API calls frontend make to backend

---
+++ frontend/src/api/rideApi.js
@@ -0,0 +1,26 @@
+
+const ROUTE = "/api/rides";
+
+export const uploadRide = async (file) => {
+    const form = new FormData();
+    form.append("file", file);
+    const res = await fetch(`${ROUTE}/upload`, { method: 'POST', body: form});
+
+    if (!res.ok) throw new Error(await res.text());
+    return res.json();
+}
+
+export const getAllRides = async () => {
+    const res = await fetch(ROUTE);
+
+    if (!res.ok) throw new Error(await res.text());
+    return res.json();
+}
+
+export const getRideByID = async (id) => {
+    const res = await fetch(`${ROUTE}/${id}`);
+
+    if (!res.ok) throw new Error(await res.text());
+    return res.json();
+}
+
